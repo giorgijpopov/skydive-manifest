2728f50d80427bdc1b10f0c39f8c954135b80aa7
Giorgiy Popov <giorgiy.popov@joom.com>
2023-07-16 12:08:17 +0100

    numerate skydivers

--- src/static/scripts.js
+++ src/static/scripts.js
@@ -1,7 +1,6 @@
 function addParachutist() {
     var table = document.getElementById("parachutists-table");
     var tr = document.createElement("tr");
-    console.log(table.rows.length)
     tr.innerHTML = `
         <td>${table.rows.length + 1}</td>
         <td><input type="text" name="parachutist" required></td>
@@ -10,8 +9,14 @@ function addParachutist() {
 }
 
 function deleteSkydiver(button) {
-    var closestTr = button.closest('tr')
+    var closestTr = button.closest('tr');
     if (closestTr) {
-        closestTr.remove()
+        closestTr.remove();
     }
+    var table = document.getElementById("parachutists-table");
+    if (!table) return;
+    for (let i = 0; i < table.rows.length; i++) {
+        table.rows[i].cells[0].textContent = i + 1;
+    }
+    rowCount = table.rows.length - 1;
 }
